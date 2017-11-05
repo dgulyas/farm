41c866dbef36be21bc3868277b41bb1690686fc5
David Gulyas <gulyman@gmail.com>
2017-11-05 16:17:07 -0700

    Add food to map
    Animals don't know how to find it or eat it yet.

--- Food.py
+++ Food.py
@@ -1,12 +1,15 @@
+from BaseThing import BaseThing
+
 class Food(BaseThing):
-	def __inti__(amount = 100, char = "f"):
+	def __init__(self, map, point, amount = 100, char = "f"):
+		super().__init__(map = map, point=point)
 		self.amount = amount
 		self.char = char
 	
-	def reduce(): #called when something takes a bite of it
+	def reduce(self): #called when something takes a bite of it
 		self.amount -= 1
 		if self.amount < 1:
 			self.deleteMe = True
 	
-	def tick():
-		pass
+	def tick(self):
+		self.amount += 1
--- World.py
+++ World.py
@@ -2,9 +2,10 @@ import os
 import sys
 import time
 from Animal import Animal
+from Food import Food
 from Map import Map
 
-def RunWorld(numAnimals = 100):
+def RunWorld(numAnimals = 100, numFood = 15):
 	
 	#print("test")
 	map = Map()
@@ -17,6 +18,13 @@ def RunWorld(numAnimals = 100):
 		animals.append(animal)
 		map.get(openPoint).value = animal
 	
+	foods = []
+	for i in range(numFood):
+		openPoint = map.randOpenPoint()
+		food = Food(map, openPoint)
+		foods.append(food)
+		map.get(openPoint).value = food
+		
 	tickNumber = 0
 	
 	while True:
@@ -33,11 +41,17 @@ def RunWorld(numAnimals = 100):
 			animal.tick()
 			if animal.deleteMe:
 				deadThings.append(animal)
+		for food in foods:
+			food.tick()
+			if food.deleteMe:
+				deadThings.append(food)
 		
 		for thing in deadThings:
 			map.get(thing.coord).value = None
 			if isinstance(thing, Animal):
 				animals.remove(thing)
+			if isinstance(thing, Food):
+				foods.remove(thing)
 		
 if __name__ == "__main__":
 	
